b220744e3f3ded4e4c1627034b7932095ca40ae3
Artem <Artem@DESKTOP-4BOPE1O>
2025-03-14 19:01:32 +0300

    Add project files.

---
+++ Patterns/FirstLesson/Singleton.cs
@@ -0,0 +1,20 @@
+﻿namespace Patterns.FirstLesson;
+
+public class Singleton
+{
+    private static Singleton singleton;
+    private Singleton()
+    {
+        
+    }
+
+    public static Singleton GetInstance()
+    {
+        if (singleton == null)
+        {
+            singleton = new Singleton();
+        }
+
+        return singleton;
+    }
+}
